5a72747a3691810a1bf4b9369ba0dd5925f504dc
Alex Desjardins <ardesjardins91@gmail.com>
2020-11-07 19:03:33 -0700

    Weather service complete

---
+++ app/services/weather_service.rb
@@ -0,0 +1,19 @@
+class WeatherService
+  def self.weather_forecast_data(coordinates = {})
+    response = conn.get('data/2.5/onecall') do |f|
+      f.params[:lat] = coordinates[:lat]
+      f.params[:lon] = coordinates[:lon]
+      f.params[:exclude] = 'minutely,alerts'
+      f.params[:units] = 'imperial'
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  def self.conn
+    Faraday.new(url: 'https://api.openweathermap.org/') do |f|
+      f.params[:appid] = ENV['WEATHER_API_KEY']
+    end
+  end
+end
---
+++ spec/services/weather_service_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe WeatherService do
+  it 'can return weather forecast when passed coordinates' do
+    VCR.use_cassette('Denver forecast') do
+      coordinates = {:lat=>39.738453, :lon=>-104.984853}
+
+      results = WeatherService.weather_forecast_data(coordinates)
+      expect(results).to be_a(Hash)
+      expect(results).to have_key(:current)
+      expect(results).to have_key(:hourly)
+      expect(results).to have_key(:daily)
+      expect(results).to_not have_key(:minutely)
+      expect(results).to_not have_key(:alerts)
+    end
+  end
+end
