a58ed626f16e9d70355b3cd60232f7ed425a3319
Alex Desjardins <ardesjardins91@gmail.com>
2020-11-09 11:49:24 -0700

    Facade refactor

--- app/facades/trail_facade.rb
+++ app/facades/trail_facade.rb
@@ -1,17 +1,24 @@
 class TrailFacade
-  def self.get_location(location)
-    location_data = LocationService.location_data(location)
-  end
-
-
-  def self.get_forecast(location)
-    location_data = get_location(location)
-    forecast_data = WeatherService.weather_forecast_data(location_data[:latLng])
-  end
+  # def self.get_location(location)
+  #   location_data = LocationService.location_data(location)
+  # end
+  #
+  #
+  # def self.get_forecast(location)
+  #   location_data = get_location(location)
+  #   forecast_data = WeatherService.weather_forecast_data(location_data[:latLng])
+  # end
+  #
+  # def self.get_trail(location)
+  #   location_data = get_location(location)
+  #   forecast_data = get_forecast(location)
+  #   trail_data = TrailService.trail_data(location_data[:latLng])
+  #   trail = Trail.new(location_data, forecast_data, trail_data)
+  # end
 
   def self.get_trail(location)
-    location_data = get_location(location)
-    forecast_data = get_forecast(location)
+    location_data = LocationService.location_data(location)
+    forecast_data = WeatherService.weather_forecast_data(location_data[:latLng])
     trail_data = TrailService.trail_data(location_data[:latLng])
     trail = Trail.new(location_data, forecast_data, trail_data)
   end
