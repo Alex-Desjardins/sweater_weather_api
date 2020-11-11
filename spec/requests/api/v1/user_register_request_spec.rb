3088eef0eacd725a10e421aee2049aec35dd0d63
Alex Desjardins <ardesjardins91@gmail.com>
2020-11-10 20:42:02 -0700

    User registration sad path

--- spec/requests/api/v1/user_register_request_spec.rb
+++ spec/requests/api/v1/user_register_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'User Registration POST request' do
-  it 'User data for registration; success' do
+  it 'User registration; success' do
     user_params = {email: 'me@user.com',
                    password: '12345',
                    password_confirmation: '12345'
@@ -16,7 +16,7 @@ RSpec.describe 'User Registration POST request' do
     user = User.last
 
     expect(response).to be_successful
-    expect(response.status).to eq(200)
+    expect(response.status).to eq(201)
     expect(response.content_type).to eq('application/json')
     expect(registration_json[:data][:type]).to eq('user')
     expect(registration_json[:data]).to have_key(:id)
@@ -29,5 +29,66 @@ RSpec.describe 'User Registration POST request' do
     expect(registration_json[:data][:attributes][:api_key]).to eq(user.api_key)
     expect(registration_json[:data][:attributes]).to_not have_key(:password)
     expect(registration_json[:data][:attributes]).to_not have_key(:password_confirmation)
+  end
+
+  it 'User registration fail; password not matching' do
+    user_params = {email: "me@user.com",
+                password: "password",
+                password_confirmation: "drowssap"}
+
+    header = { "CONTENT_TYPE" => "application/json" }
+
+    post '/api/v1/users', headers: header, params: JSON.generate(user_params)
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(422)
+    expect(response.content_type).to eq('application/json')
+
+    expect(User.all.count).to eq(0)
+
+    request_json = JSON.parse(response.body, symbolize_names: true)
+    expect(request_json[:error]).to eq("Password confirmation doesn't match Password")
+  end
+
+  it 'User registration fail; email currently in use' do
+    existing_user = User.create!(email: "me@user.com",
+                                 password: "password",
+                                 password_confirmation: "password")
+
+    user_params = {email: "me@user.com",
+                password: "password",
+                password_confirmation: "password"}
+
+    header = { "CONTENT_TYPE" => "application/json" }
+
+    post '/api/v1/users', headers: header, params: JSON.generate(user_params)
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(422)
+    expect(response.content_type).to eq('application/json')
+
+    expect(User.all.count).to eq(1)
+
+    request_json = JSON.parse(response.body, symbolize_names: true)
+    expect(request_json[:error]).to eq("Email has already been taken")
+  end
+
+  it 'User registration fail; missing field' do
+    user_params = {email: "",
+                password: "",
+                password_confirmation: ""}
+
+    header = { "CONTENT_TYPE" => "application/json" }
+
+    post '/api/v1/users', headers: header, params: JSON.generate(user_params)
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(422)
+    expect(response.content_type).to eq('application/json')
+
+    expect(User.all.count).to eq(0)
+
+    request_json = JSON.parse(response.body, symbolize_names: true)
+    expect(request_json[:error]).to eq("Email can't be blank, Password can't be blank, and Password can't be blank")
   end
 end
