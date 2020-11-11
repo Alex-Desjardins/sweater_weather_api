97295d526570ed754c82430fbd7ef5b81256fe8a
Alex Desjardins <ardesjardins91@gmail.com>
2020-11-10 21:21:45 -0700

    User login sad path

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: session_params[:email])
-    if user && user.authenticate(session_params[:password])
+
+    if session_params[:email].empty? || session_params[:password].empty?
+      render json: { error: 'Please Fill In All Fields' }, status: 401
+    elsif user.nil? || user && !user.authenticate(session_params[:password])
+      render json: { error: 'Incorrect Email or Password' }, status: 401
+    elsif user && user.authenticate(session_params[:password])
       render json: UserSerializer.new(user), status: 201
     end
   end
